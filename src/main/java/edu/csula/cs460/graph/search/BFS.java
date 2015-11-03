390d286dce6914d7b36e25bf3e68728d35b22848
Vicken52 <krikorian.vicken@gmail.com>
2015-11-03 07:59:50 -0800

    Transferring work to laptop

--- src/main/java/edu/csula/cs460/graph/search/BFS.java
+++ src/main/java/edu/csula/cs460/graph/search/BFS.java
@@ -4,10 +4,51 @@ import edu.csula.cs460.graph.Edge;
 import edu.csula.cs460.graph.Graph;
 import edu.csula.cs460.graph.Node;
 
-import java.util.List;
+import java.util.*;
 
 public class BFS implements SearchStrategy {
     public List<Edge> search(Graph graph, Node source, Node dist) {
+
+        List<Edge> result = null;
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+
+            for(Node node : graph.neighbors(n))
+            {
+                if(node.getId() != dist.getId())
+                {
+                    queue.add(node);
+                }
+                else
+                {
+                    if(result != null)
+                    {
+                        result.add(0, new Edge(n, node, graph.distance(n, node)));
+                    }
+                    else
+                    {
+                        result = new ArrayList<>();
+                        result.add(new Edge(n, node, graph.distance(n, node)));
+                    }
+
+                    if(n == source)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        dist = node;
+                        queue.clear();
+                        queue.add(source);
+                    }
+                }
+            }
+        }
+
         return null;
     }
 }
