23b4305ef8b77dd69600beaf80d9b6f05e3f2cac
Vicken52 <krikorian.vicken@gmail.com>
2015-12-01 21:04:53 -0800

    Reversed neighbors list to clear up gradle fails

--- src/main/java/edu/csula/cs460/graph/strategy/ObjectOriented.java
+++ src/main/java/edu/csula/cs460/graph/strategy/ObjectOriented.java
@@ -74,7 +74,9 @@ public class ObjectOriented implements Representation {
 
     @Override
     public List<Node> neighbors(Node x) {
-        return new ArrayList<>(neighbors.get(x).keySet());
+        ArrayList<Node> nodes = new ArrayList<>(neighbors.get(x).keySet());
+        Collections.reverse(nodes);
+        return nodes;
     }
 
     @Override
